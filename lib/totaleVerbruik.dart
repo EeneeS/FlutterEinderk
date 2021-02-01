70186878cc961fd3c6ec27fd24af3c89060048cd
Niels Soete <soeteniels1803@gmail.com>
2021-02-01 22:58:26 +0100

    commit

--- lib/totaleVerbruik.dart
+++ lib/totaleVerbruik.dart
@@ -1,7 +1,4 @@
-import 'dart:async';
-import 'API.dart';
 import 'package:flutter/material.dart';
-import 'dart:convert';
 
 class TotaleVerbruik extends StatefulWidget {
   @override
@@ -10,24 +7,7 @@ class TotaleVerbruik extends StatefulWidget {
 
 class _TotaleVerbruikState extends State<TotaleVerbruik> {
 
-  Timer timer;
   String _totaleVerbruik = 'No data';
-  String url = 'http://192.168.0.124:5000/';
-  var data;
-
-  @override
-  void initState() {
-    super.initState();
-    timer = Timer.periodic(Duration(seconds: 1), (timer) => updateValue());
-  }
-
-  void updateValue() async {
-    data = await getData(url);
-    var decodedData = jsonDecode(data);
-    setState(() {
-      _totaleVerbruik = decodedData['vermogen'].toString();
-    });
-  }
 
   static const TextStyle optionsStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
